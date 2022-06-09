5e853da65f2a33857bdd23ae5f2d2483badae4af
alishaburgfeld <alisha.burgfeld@gmail.com>
2022-06-09 18:00:55 -0400

    fixed one thing

--- python/binary_search.py
+++ python/binary_search.py
@@ -3,11 +3,10 @@ import random
 # values = random.sample(list(range(1,10000)),10)
 # print(values)
 values=[1,2,3,4,5,6,7,8,9,10]
-def binary_search(values,target,low=0,high=len(values)):
+def binary_search(values,target,low=0,high=len(values)-1):
     values.sort()
     mid= round((low + high) /2)
-    print(f"low and high: {low}, {high}, {values[low:high]} ,middle  {mid}, value at index middle{values[mid]}")
-    # print(values[mid], "middle")
+    # print(f"low and high: {low}, {high}, {values[low:high]} ,middle  {mid}, value at index middle{values[mid]}")
     if target == values[mid]:
         return mid
     
@@ -18,7 +17,7 @@ def binary_search(values,target,low=0,high=len(values)):
     elif target > values[mid]:
         return binary_search(values,target,mid+1,high)
 
-print(binary_search(values,8))
+print(binary_search(values,12))
 # values.sort() # We now have a values list of 1000 unique values between 1 and 10,000
 
 # binary_search(537, values) # this returns the index of 537 in values if it exists and -1 if it doesn't exist
