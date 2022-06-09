b3a20a5c0352f541dd20535ee08b9da830724e6c
alishaburgfeld <alisha.burgfeld@gmail.com>
2022-06-09 17:58:36 -0400

    added py solution

--- python/binary_search.py
+++ python/binary_search.py
@@ -1,2 +1,24 @@
-def binary_search():
-    pass
+import random
+
+# values = random.sample(list(range(1,10000)),10)
+# print(values)
+values=[1,2,3,4,5,6,7,8,9,10]
+def binary_search(values,target,low=0,high=len(values)):
+    values.sort()
+    mid= round((low + high) /2)
+    print(f"low and high: {low}, {high}, {values[low:high]} ,middle  {mid}, value at index middle{values[mid]}")
+    # print(values[mid], "middle")
+    if target == values[mid]:
+        return mid
+    
+    elif len(values[low:high]) == 1:
+        return -1
+    elif target < values[mid]:
+        return binary_search(values,target,low,mid)
+    elif target > values[mid]:
+        return binary_search(values,target,mid+1,high)
+
+print(binary_search(values,8))
+# values.sort() # We now have a values list of 1000 unique values between 1 and 10,000
+
+# binary_search(537, values) # this returns the index of 537 in values if it exists and -1 if it doesn't exist
---
+++ python/tempCodeRunnerFile.py
@@ -0,0 +1,14 @@
+def binary_search(values,target,low=0,high=len(values)):
+#     sorted=values.sort()
+#     mid= round(len(sorted) /2)
+#     if target == mid:
+#         return True
+    
+#     elif values.length == 1:
+#         return -1
+#     elif target < mid:
+#         binary_search(sorted,target,0,mid)
+#     elif target > mid:
+#         binary_search(sorted,target,mid,len(sorted))
+
+# print(binary_search(values,750))
